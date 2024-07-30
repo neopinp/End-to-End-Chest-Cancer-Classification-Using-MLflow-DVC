d17c0475ab56e343c70dc0cca41acffb39efbabb
neopinp <Neo.Pi1@marist.edu>
2024-07-29 20:28:39 -0400

    logging

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,23 @@
+import os 
+import sys 
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs" # create "logs" directory 
+log_filepath = os.path.join(log_dir, "running_logs.log") # save all logs inside dir 
+os.makedirs(log_dir, exist_ok=True) # creating folder 
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = logging_str,
+
+    handlers = [
+        logging.fileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
+
+

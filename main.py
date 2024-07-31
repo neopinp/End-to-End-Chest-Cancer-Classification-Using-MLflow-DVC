c4862e324a9b178054f4bb275323ece7c9ee39ea
neopinp <Neo.Pi1@marist.edu>
2024-07-31 00:01:00 -0400

    Data Ingestion

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-# test logging 
-from src.cnnClassifier import logger 
+# testing
 
-logger.info("Welcome to cnnClassifier")
+from src.cnnClassifier import logger 
+from src.cnnClassifier.pipeline.stage_01 import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f'>>> stage {STAGE_NAME} started <<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e

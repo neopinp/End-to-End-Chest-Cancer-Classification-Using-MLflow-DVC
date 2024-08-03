b1e38ceb95902f3762961d5fdeba2303ba25eabc
neopinp <Neo.Pi1@marist.edu>
2024-08-03 01:26:05 -0400

    + dvc pipeline

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
         logger.info(f"*******************")
         logger.info(f">>> stage {STAGE_NAME} started <<<")
         obj = PrepareBaseModelTrainingPipeline()
-        obj.main
+        obj.main()
         logger.info(">>> stage {STAGE_NAME} completed <<<\n\nx===============x")
     except Exception as e:
         logger.exception(e)

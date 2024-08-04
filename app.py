b366c27706af3156fd128ad293af53ab9bcecbf1
neopinp <Neo.Pi1@marist.edu>
2024-08-04 00:52:37 -0400

    model added

--- app.py
+++ app.py
@@ -5,37 +5,49 @@ from src.cnnClassifier.utils.common import decodeImage
 from src.cnnClassifier.pipeline.prediction import PredictionPipeline
 
 
-os.putenv("LANG", "en_US.UTF-8")
-os.putenv("LC_ALL", "en_US.UTF-8")
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
 
-class ClientApp: 
+
+class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
 
-@app.route("/", methods = ["GET"])
+
+
+
+@app.route("/", methods=['GET'])
 @cross_origin()
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/train", methods = ["GET", "POST"])
+
+
+@app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
     os.system("python main.py")
-    return "Training done successfully"
+    # os.system("dvc repro")
+    return "Training done successfully!"
 
-@app.route("/predict", methods = ["POST"])
+
+
+
+@app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
-    image = request.json("[image]")
+    image = request.json['image']
     decodeImage(image, clApp.filename)
     result = clApp.classifier.predict()
     return jsonify(result)
 
-if __name__ == "__main__": 
+
+
+if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host = "0.0.0.0", port = 8080)
+    app.run(host='0.0.0.0', port=8080) #for AWS
--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -1,18 +1,23 @@
 import numpy as np
-import tensorflow as tf 
-from tensorflow.keras.model import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
 
-class PredictionPipeline: 
-    def __init__(self, filename): 
-        pass 
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
 
-    def predict(self): 
-        model  = load_model(os.path.join("model", "model.h5"))
 
-        imagename = self.filename 
+    
+    def predict(self):
+        ## load model
+        
+        # model = load_model(os.path.join("artifacts","training", "model.h5"))
+        model = load_model(os.path.join("model", "model.h5"))
+
+        imagename = self.filename
         test_image = image.load_img(imagename, target_size = (244,244))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
@@ -20,10 +25,8 @@ class PredictionPipeline:
         print(result)
 
         if result[0] == 1:
-            prediction = "Normal"
-            return [{"image": prediction}]
+            prediction = 'Normal'
+            return [{ "image" : prediction}]
         else:
-            prediction = "Adenocarcinoma Cancer"
-            return [{"image": prediction}]
-        
-
+            prediction = 'Adenocarcinoma Cancer'
+            return [{ "image" : prediction}]
